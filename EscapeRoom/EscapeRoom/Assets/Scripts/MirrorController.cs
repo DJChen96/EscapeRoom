b1d6584361a9b04e7c31ee8606a0a161ad381f7e
baoxiao_tong <baoxiao_tong@hotmail.com>
2018-11-29 16:43:19 +0000

    add mirror head

--- EscapeRoom/EscapeRoom/Assets/Scripts/MirrorController.cs
+++ EscapeRoom/EscapeRoom/Assets/Scripts/MirrorController.cs
@@ -1,70 +1,79 @@
-﻿using System.Collections;
+﻿using RogoDigital.Lipsync;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MirrorController : MonoBehaviour {
 
-    public AudioClip[] stageAudioClip;
-    public GameObject masksUp;
-    public GameObject masksDown;
-    public AudioSource audioSource;
+    public LipSyncData[] lipSyncDatas;
+    public GameObject mirror_head;
+    public Transform mirror_head_appear;
+    public Transform mirror_head_disappear;
+    public LipSync lipSync;
+
     private int stage = 0;
-    private bool speaking = false;
+    public bool speaking = false;
 
-    private Vector3[] originPosition = new Vector3[2];
-    // Use this for initialization
+    public bool isRunning = false;
+
+    public bool HeadAppear = false;
+    public bool HeadDisappear = false;
     void Start () {
-        originPosition[0] = masksUp.transform.position;
-        originPosition[1] = masksDown.transform.position;
-
-
     }
 	
     public void SetStage (int _stage)
     {
         this.stage = _stage;
         MirrorSpeak();
-
     }
 
     
     public void MirrorSpeak()
     {
-        if (speaking) return;
+        if (speaking)
+            return;
 
-        if (stage == 0 || stage > 5) return;
+        if (stage == 0 || stage > 5)
+            return;
 
-        audioSource.clip = stageAudioClip[stage-1];
-        audioSource.Play();
-        speaking = true;
-
+        
+        HeadAppear = true;
+        
     }
 
 
-	// Update is called once per frame
+    // Update is called once per frame
     private float mask_mouth_moving_time = 0;
 	void Update () {
-        if (!audioSource.isPlaying && speaking)
+        if (!lipSync.IsPlaying && speaking && !HeadAppear)
         {
-            speaking = false;
+            HeadDisappear = true;
+        }
+         
+        if (HeadAppear)
+        {
+            speaking = true;
+            mirror_head.transform.position =
+            Vector3.Lerp(mirror_head.transform.position, mirror_head_appear.position, Time.deltaTime * 2.5f);
+            if (Vector3.Distance(mirror_head.transform.position, mirror_head_appear.position) < 0.01f)
+            {
+                HeadAppear = false;
+                lipSync.Play(lipSyncDatas[stage - 1]);
+                
+            }
         }
 
-        if (speaking)
+        if (HeadDisappear)
         {
-            mask_mouth_moving_time += Time.deltaTime * 10f;
+            mirror_head.transform.position =
+                Vector3.Lerp(mirror_head.transform.position, mirror_head_disappear.position, Time.deltaTime * 2.5f);
+            if (Vector3.Distance(mirror_head.transform.position, mirror_head_disappear.position) < 0.01f)
+            {
+                HeadDisappear = false;
+                speaking = false;
+            }
 
-            //masksUp.transform.position = new Vector3(originPosition[0].x, originPosition[0].y + 0.01f + 0.02f *Mathf.Sin(mask_mouth_moving_time), originPosition[0].z);
-            //masksDown.transform.position = new Vector3(originPosition[0].x, originPosition[0].y - 0.01f - 0.02f * Mathf.Sin(mask_mouth_moving_time), originPosition[0].z);
-            masksUp.transform.position = new Vector3(originPosition[0].x, originPosition[0].y + 0.02f, originPosition[0].z);
-            masksDown.transform.position = new Vector3(originPosition[1].x, originPosition[1].y - 0.02f, originPosition[1].z);
         }
-        else
-        {
-            masksUp.transform.position = originPosition[0];
-            masksDown.transform.position = originPosition[1];
-        }
-
-
 
         if (gameController.debugMode == false)
             return;
